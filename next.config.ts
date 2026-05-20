5fcd9ce9c53e4889a2c1ff31527458f4c218ae21
maxlevanogthb <mlevmark@gmail.com>
2026-05-20 17:12:32 -0600

    feat: forzado de creacion landing page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // <-- Esto obliga a Next.js a compilar la landing como archivos HTML puros en una carpeta 'out'
+  images: {
+    unoptimized: true, // Requerido por Next.js cuando usas output export
+  }
 };
 
 export default nextConfig;
